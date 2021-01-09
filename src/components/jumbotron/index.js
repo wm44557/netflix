c2c9d4a95074505971e90b2e6631669bd74b617a
Marcin Wrzos <marwrz36@gmail.com>
2021-01-09 15:19:55 +0100

    global styles, jumbotron component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Pane,Image,SubTitle,Title,Container,Item,Inner} from "./styles/jumbotron";
+
+export default function Jumbotron({children,direction = 'row',...restProps}) {
+    return (
+        <Item direction={direction}>
+            <Inner>
+                {children}
+            </Inner>
+        </Item>
+    )
+};
+
+Jumbotron.Container = function JumbotronContainer({children,...restProps}){
+    return <Container {...restProps}>{children}</Container>
+}
+Jumbotron.Pane = function JumbotronPane({children,...restProps}){
+    return <Pane {...restProps}>{children}</Pane>
+}
+Jumbotron.Title = function JumbotronPane({children,...restProps}){
+    return <Title {...restProps}>{children}</Title>
+}
+Jumbotron.SubTitle = function JumbotronSubTitle({children,...restProps}){
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+Jumbotron.Image = function JumbotronImage({...restProps}){
+    return <Image {...restProps}/>
+}
