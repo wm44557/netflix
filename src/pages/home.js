909571c27d7903a3fcd1b3bebb09ff56ce6d7215
Marcin Wrzos <marwrz36@gmail.com>
2021-01-11 15:58:08 +0100

    feature component, logo, netflix homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Feature, OptForm } from '../components';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faq';
 import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, Tv programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email adress" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
