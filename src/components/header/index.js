05061076f90ae35bb598140877858d403ce7012f
Marcin Wrzos <marwrz36@gmail.com>
2021-01-14 10:09:11 +0100

    styled profile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import { Link as ReactRouterLink } from 'react-router-dom';
 import { Background, ButtonLink, Container, Logo } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
-  return bg ? <Background {...restProps}>{children}</Background> : { children };
+  return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
